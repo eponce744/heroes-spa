39b7bc221f51001e68ad8fa6d945e7a758d6f00d
Ernesto <rponce831@alumnos.iua.edu.ar>
2025-01-15 14:34:24 -0300

    Login de un usuario

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import AuthProvider from '../../auth/context/AuthProvider';
+import { useContext } from 'react';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
 
     const navigate = useNavigate();
+
+    const { user } = useContext(AuthContext);
+    console.log(user);
+
 
     const onLogout = () => {
         navigate('/login', {
@@ -11,6 +18,7 @@ export const Navbar = () => {
         })
     }
 
+    
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -53,7 +61,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Ernesto
+                        {user?.name}
                     </span>
 
                     <button
